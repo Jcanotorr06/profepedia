e58a94b022d65c678d4c81556a1f2c64d4259195
Jcanotorr06 <jcano062001@gmail.com>
2022-07-20 00:50:36 -0500

    update: remove useless middleware

--- pages/middleware.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export function middleware(request:NextRequest) {
-    console.log(request)
-    return NextResponse.json({'hello': 'world'})
-}
