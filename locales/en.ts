cd0e8740fba706d475b21d3dade577540716fde8
Jcanotorr06 <jcano062001@gmail.com>
2022-07-23 12:42:39 -0500

    translation: add new translations

--- locales/en.ts
+++ locales/en.ts
@@ -3,7 +3,13 @@ const messages = {
     "logout": "Logout",
     "login": "LogIn",
     "kofi_message": "Help us keep this page online 🥰",
-    "kofi_button": "Support us on Ko-fi"
+    "kofi_button": "Support us on Ko-fi",
+    "home": "Home",
+    "about": "About",
+    "contact": "Contact",
+    "faq": "FAQ",
+    "faq_title": "Frequently Asked Questions",
+    "search_placeholder": 'Search Professor'
 }
 
 export default messages
--- locales/es.ts
+++ locales/es.ts
@@ -3,7 +3,13 @@ const messages = {
     "logout": "Cerrar Sesión",
     "login": "Acceder",
     "kofi_message": "Ayudanos a mantener esta página online 🥰",
-    "kofi_button": "Apoyanos en Ko-fi"
+    "kofi_button": "Apoyanos en Ko-fi",
+    "home": "Inicio",
+    "about": "Acerca de",
+    "contact": "Contacto",
+    "faq": "FAQ",
+    "faq_title": "Preguntas Frecuentes",
+    "search_placeholder": 'Buscar Profesor'
 }
 
 export default messages
