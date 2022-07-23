2321033b98ad2b09d6d9dcfa05cd1b87f8fff95d
Jcanotorr06 <jcano062001@gmail.com>
2022-07-23 12:51:41 -0500

    update: add new contexts and progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../styles/cursor.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
@@ -7,9 +8,24 @@ import { ModeProvider, useMode } from '../context/ModeContext'
 import { LocaleProvider } from '../context/LocaleContext'
 import DefaultLayout from '../layouts/default'
 import { TopMessage } from '../components/Notifications'
+import { useEffect } from 'react';
+import { disableReactDevTools } from '../utils/disableReactDevTools'
+import { ModalProvider, SearchProvider } from '../context'
+import { useModal } from './../context/ModalContext';
+import NextNProgress from 'nextjs-progressbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   
+  useEffect(() => {
+    /* if(process.env.NODE_ENV === 'production') {
+      disableReactDevTools()
+      console.log = () =>{}
+      console.error = () =>{}
+      console.warn = () =>{}
+      console.info = () =>{}
+    } */
+  }, [])
+
   const { mode } = useMode()
 
   return (
@@ -22,10 +38,15 @@ function MyApp({ Component, pageProps }: AppProps) {
       <UserProvider>
         <ModeProvider>
           <LocaleProvider>
-            <TopMessage/>
-            <DefaultLayout>
-              <Component {...pageProps}/>
-            </DefaultLayout>
+            <ModalProvider>
+              <NextNProgress height={6} color="#6C4BDA"/>
+              <TopMessage/>
+              <SearchProvider>
+                <DefaultLayout>
+                  <Component {...pageProps}/>
+                </DefaultLayout>
+              </SearchProvider>
+            </ModalProvider>
           </LocaleProvider>
         </ModeProvider>
       </UserProvider>
