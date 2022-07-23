715313611159da0cb79cb1e549198ba05711dca4
Jcanotorr06 <jcano062001@gmail.com>
2022-07-23 12:50:54 -0500

    update: implement modal on default layout

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react"
+import { Modal } from "../components/Modals"
 import { Footer, MobileNavBar, NavBar } from "../components/Navigation"
-import { useMode } from "../context/ModeContext"
+import { useMode } from "../context"
 
 type Props = {
     children: ReactNode
@@ -11,16 +12,19 @@ const DefaultLayout = ({children}:Props) => {
     const { mode } = useMode()
 
     return (
-        <div className={`${mode} gradient-background min-h-screen h-full flex flex-col`}>
+    <>
+        <Modal/>
+        <div className={`${mode} body h-full flex flex-col`}>
             <header className="w-full">
                 <NavBar/>
                 <MobileNavBar/>
             </header>
-            <main className="pt-4 container mx-auto min-h-full grow">
+            <main className="py-4 container mx-auto grow">
                 {children}
             </main>
             <Footer/>
         </div>
+    </>
     )
 }
 
