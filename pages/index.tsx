d8bdbec7dafa3787c71096282c60f53221073637
Jcanotorr06 <jcano062001@gmail.com>
2022-08-07 17:54:22 -0500

    update: add test components

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,15 @@ import Link from 'next/link';
 
 //Import Images
 import Business from '../public/landing/business.svg'
+import { toast } from 'react-toastify'
+import Load from 'react-loading';
+import { IconButton } from '../components/Buttons'
 
 const Home: NextPage = () => {
+
+  const notify = () => {
+    toast.error("Ha ocurrido un error")
+  }
   return (
     <>
       <Head>
@@ -18,12 +25,23 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="grid grid-cols-12 gap-2 w-full min-h-full justify-center relative surface">
-        <div className="border border-red-500 col-span-12 lg:col-span-5">
+        <div className="border border-red-500 col-span-12 lg:col-span-5 p-3">
           <Link href="/docente/792">
             <a>
               Docente 792
             </a>
           </Link>
+          <div>
+            <button onClick={() => notify()} className="bg-blue-300 p-3">Toast</button> 
+          </div>
+          <div>
+            <Load color='#000' type="bubbles"/>
+          </div>
+          <div className=''>
+            <div className="flex justify-between w-full rounded-full input-main input shadow-lg border">
+              <input type="text" className="px-3 py-10 text-lg rounded-full w-full input_transparent" placeholder="Nombre del profesor" />
+            </div>
+          </div>
         </div>
         <div className="border border-blue-500 col-span-12 lg:col-span-7 justify-center flex">
           <div className='block w-full' style={{maxWidth: '600px'}}>
