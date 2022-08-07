b8387425ceca22a4e51a6da5733acccb9b39599d
Jcanotorr06 <jcano062001@gmail.com>
2022-08-07 17:45:38 -0500

    translations: add new translations

--- locales/en.ts
+++ locales/en.ts
@@ -9,7 +9,15 @@ const messages = {
     "contact": "Contact",
     "faq": "FAQ",
     "faq_title": "Frequently Asked Questions",
-    "search_placeholder": 'Search Professor'
+    "search_placeholder": 'Search Professor',
+    "login_title": "Welcome to Profepedia",
+    "login_message": "Enter your student email and we will send you a one time login link.",
+    "invalid_email": "Invalid email address. You must use a \"@utp.ac.pa\" email.",
+    "send_auth_email": "Send Login Link",
+    "login_disclaimer": "Exclusive access for Technological Univeristy of Panama students.",
+    "login_email_ph": "Student email address",
+    "login_success_title": "Authentication Link Sent Successfully",
+    "login_success_message": "Please check your email and click on the link that has just been sent to finish the login process.",
 }
 
 export default messages
--- locales/es.ts
+++ locales/es.ts
@@ -9,7 +9,15 @@ const messages = {
     "contact": "Contacto",
     "faq": "FAQ",
     "faq_title": "Preguntas Frecuentes",
-    "search_placeholder": 'Buscar Profesor'
+    "search_placeholder": 'Buscar Profesor',
+    "login_title": "Bienvenido a Profepedia",
+    "login_message": "Ingresa tu correo universitario y te enviaremos un link de un solo uso para iniciar sesión.",
+    "invalid_email": "Dirección de correo invalida. Debes utilizar un correo \"@utp.ac.pa\"",
+    "send_auth_email": "Enviar Link de Acceso",
+    "login_disclaimer": "Acceso exclusivo para estudiantes de la Universidad Tecnológica de Panamá.",
+    "login_email_ph": "Dirección de correo universitaria",
+    "login_success_title": "Link de Autenticación Enviado Exitosamente",
+    "login_success_message": "Revisa tu correo electronico y haz clic en el link que te enviamos para iniciar sesión.",
 }
 
 export default messages
