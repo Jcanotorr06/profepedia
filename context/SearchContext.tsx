b730b291f3fe53cc72c5118ae0ed8bd786b492c0
Jcanotorr06 <jcano062001@gmail.com>
2022-08-07 17:52:29 -0500

    update: update search functionality to limit results and add load more

--- context/SearchContext.tsx
+++ context/SearchContext.tsx
@@ -1,38 +1,33 @@
 import { createContext, ReactNode, useContext, useState, useEffect } from "react"
 import { useRouter } from 'next/router';
-import { NextURL } from "next/dist/server/web/next-url";
 import { supabase } from './../utils/supabaseClient';
+import { searchResult } from "../types/searchResult";
 
-type searchResult = {
-    cant_rating: number,
-    cant_wouldTakeAgain: number,
-    departamento: string | null | undefined,
-    dificultad: number,
-    id: number,
-    id_universidad: number,
-    nombre: string,
-    rating: number,
-    unidad: string | null | undefined
-}
 
 type searchContext = {
     query: string,
     previousQuery: string,
     searchResult: searchResult[],
+    amount: number,
     loading: boolean,
     success: boolean,
     failure: boolean,
-    handleSearch: (query:string) => void
+    handleSearch: (query:string) => void,
+    loadMore: () => void,
+    loadingMore: boolean
 }
 
 const searchContextDefault:searchContext = {
     query: '',
     previousQuery: '',
     searchResult: [],
+    amount: 0,
     loading: false,
     success: false,
     failure: false,
-    handleSearch: (query:string) => {}
+    handleSearch: (query:string) => {},
+    loadMore: () => {},
+    loadingMore: false
 }
 
 const SearchContext = createContext<searchContext>(searchContextDefault)
@@ -52,21 +47,44 @@ export function SearchProvider({children}:Props) {
     const [loading, setLoading] = useState<boolean>(false)
     const [success, setSuccess] = useState<boolean>(false)
     const [failure, setFailure] = useState<boolean>(false)
+    const [amount, setAmount] = useState<number>(0)
+    const [loadingMore, setLoadingMore] = useState<boolean>(false)
     const router = useRouter()
 
     useEffect(() => {
 
     }, [query])
 
-    const search = async (q:string) => {
-        setLoading(true)
-        const { data, error } = await supabase.rpc("busqueda_docentes_full", {nom: q})
+    const search = async (q:string, limit: number, rangeStart:number, rangeEnd:number) => {
+        /* setLoading(true) */
+        /* const { data, error } = await supabase.rpc("busqueda_docentes_full", {nom: q, lim: 10, offst: 0}) */
+        const { data, error, count } = await supabase.from("resultado_busqueda_v2").select("*", {count: "exact"}).textSearch("nombre", q, {type: "websearch"}).limit(limit).range(rangeStart,rangeEnd)
         if(error){
             console.log(error)
             return false
         }
-        console.log(data)
-        return data
+        console.log(data, count)
+        return {
+            data,
+            count
+        }
+    }
+
+    const loadMore = async () => {
+        setLoadingMore(true)
+        let res = await search(query, 10, searchResult.length, searchResult.length+9)
+        if(res) {
+            let data = res.data
+            setSearchResult(sr => [...sr, ...data])
+            setSuccess(true)
+            setFailure(false)
+            setLoadingMore(false)
+        }else{
+            setSearchResult(sr => sr)
+            setSuccess(false)
+            setFailure(true)
+            setLoading(false)
+        }
     }
 
     const handleSearch = async (q:string) => {
@@ -74,11 +92,13 @@ export function SearchProvider({children}:Props) {
             console.log('SAME QUERY', q)
             console.log('PREVIOUS RESPONSE', searchResult)
         }else{
+            setLoading(true)
             setPreviousQuery(query)
             setQuery(q)
-            let res = await search(q)
+            let res = await search(q, 10, 0, 9)
             if(res) {
-                setSearchResult(res)
+                setSearchResult(res.data)
+                setAmount(res.count ?? 0)
                 setSuccess(true)
                 setFailure(false)
                 setLoading(false)
@@ -99,10 +119,13 @@ export function SearchProvider({children}:Props) {
         query,
         previousQuery,
         searchResult,
+        amount,
         loading,
         success,
         failure,
-        handleSearch
+        handleSearch,
+        loadMore,
+        loadingMore
     }
 
     return (
