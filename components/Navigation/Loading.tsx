41570a4dde928865ca172a4907478571f6d81262
Jcanotorr06 <jcano062001@gmail.com>
2022-08-07 17:47:25 -0500

    update: create component for loading overlay

---
+++ components/Navigation/Loading.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from 'react'
+import Load from 'react-loading';
+
+interface Props{
+    children: ReactNode,
+    active: boolean,
+    className: string
+}
+
+const Loading = ({ children, active, className }:Props) => {
+  return (
+    <>
+        {active ? 
+            <div className={`absolute z-10 top-0 left-0 bg-black bg-opacity-30 flex justify-center items-center ${className}`}>
+                <Load type="spin" color="#fff"/>
+            </div>:
+            children
+        }
+    </>
+  )
+}
+
+export default Loading
--- components/Navigation/index.ts
+++ components/Navigation/index.ts
@@ -1,3 +1,4 @@
 export { default as NavBar } from './NavBar'
 export { default as MobileNavBar } from './MobileNavBar'
 export { default as Footer } from './Footer'
+export {default as Loading} from './Loading'
