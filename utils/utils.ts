bc614bd5b7981c4c6b4baad1021979b7aaf93efd
Jcanotorr06 <jcano062001@gmail.com>
2022-08-07 17:51:26 -0500

    update: create functions to format professor data

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,5 +1,7 @@
 export {
-    toTitleCase
+    toTitleCase,
+    formatNombre,
+    formatGroup
 }
 
 const toTitleCase = (phrase:string) => {
@@ -9,3 +11,11 @@ const toTitleCase = (phrase:string) => {
       .map(word => word.charAt(0).toUpperCase() + word.slice(1))
       .join(' ');
 };
+
+const formatNombre = (nombre:string) => {
+    return toTitleCase(`${nombre.split(', ')[1]} ${nombre.split(', ')[0].split(' ')[0]}`)
+}
+
+const formatGroup = (group:string) => {
+    return toTitleCase(group.trimStart())
+}
