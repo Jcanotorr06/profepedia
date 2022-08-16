ba850d2cb643c9355e74f3c8caf46bacc55c3d35
Jcanotorr06 <jcano062001@gmail.com>
2022-08-15 21:49:12 -0500

    types: add new modal types

--- types/modalTypes.ts
+++ types/modalTypes.ts
@@ -1,1 +1,5 @@
-export type modalTypes = 'LOGIN' | 'LOGIN_CONFIRMATION' | 'LOGOUT' | 'LOGOUT_CONFIRMATION' | ''
+export type modalTypes = 'LOGIN' | 'LOGIN_CONFIRMATION' | 'LOGOUT' | 'LOGOUT_CONFIRMATION' | 'REVIEW' | 'REVIEW_CONFIRMATION' | 'REVIEW_SUCCESS' | ''
+
+export type modalProps = {
+    size: 'default' | 'full',
+}
