c3f2b58bfd1cd731f52480532de7171442a9aff0
Joseph Cano <joseph@paguelofacil.com>
2022-09-03 02:46:18 -0500

    config: update supabaseClient to add options

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -3,4 +3,4 @@ import { createClient} from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_API_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_API_KEY!
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {multiTab: false, autoRefreshToken: true, persistSession: true, detectSessionInUrl: true})
