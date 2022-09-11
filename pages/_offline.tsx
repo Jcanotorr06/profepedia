1f45aade915984bf20f2dbaa11994ab575607f4a
Jcanotorr06 <jcano062001@gmail.com>
2022-09-11 16:38:13 -0500

    update: update offline page

--- pages/_offline.tsx
+++ pages/_offline.tsx
@@ -1,9 +1,23 @@
 import { NextPage } from "next"
+import { useIntl } from 'react-intl';
+import Head from "next/head";
 
 
 const OfflinePage:NextPage = () => {
+    const intl = useIntl()
     return (
-      <div>OfflinePage</div>
+      <>
+        <Head>
+          <title>Offline | Profepedia</title>
+          <meta name="description" content="Conectate a internet para acceder a la pagina" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main className="w-screen h-screen overflow-hidden flex justify-center items-center">
+          <div className="p-2 text-center">
+            <h1>You&apos;re Offline</h1>
+          </div>
+        </main>
+      </>
     )
   }
   
