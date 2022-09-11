2e190d31325ba894e5bb2396bbc5fda900810714
Jcanotorr06 <jcano062001@gmail.com>
2022-09-11 16:27:23 -0500

    update: move font import to _document.tsx to allow for font optimization on load

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,14 @@ class MyDocument extends Document {
           <Head>
             <link rel="manifest" href="/manifest.json" />
             <link rel="apple-touch-icon" href="/icon.png"></link>
+            <link 
+              href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,800;1,900&display=swap"
+              rel="stylesheet"  
+            />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400&display=swap"
+              rel="stylesheet"
+            />
             <meta name="theme-color" content="#fff" />
             <meta name="description" content="Profepedia. Help other students by rating and reviewing your professors" />
           </Head>
