b5c88bda350b1a963ed8137bd050484ad98742f0
Jcanotorr06 <jcano062001@gmail.com>
2022-09-17 00:22:46 -0500

    update: add getStaticProps to landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -68,4 +68,10 @@ const Home: NextPage = () => {
   )
 }
 
+export async function getStaticProps() {
+  return {
+    props: {}, // will be passed to the page component as props
+  }
+}
+
 export default Home
