86c7fd901e5f3a79ed92be500a94fa8696d0b21f
Jcanotorr06 <jcano062001@gmail.com>
2022-09-17 00:39:29 -0500

    feat: configure purgecss to remove unused css on build

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,14 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    '@fullhuman/postcss-purgecss': {
+      content: [
+          './pages/**/*.{js,jsx,ts,tsx}',
+          './components/**/*.{js,jsx,ts,tsx}',
+          './layouts/**/*.{js,jsx,ts,tsx}',
+      ],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+      safelist: ["html", "body"]
+    },
   },
 }
