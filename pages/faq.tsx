bcaee23518791ef4ad224ac9a3ad9e2e3fce5bc6
Jcanotorr06 <jcano062001@gmail.com>
2022-09-17 22:28:52 -0500

    update: finish faq page

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -4,62 +4,26 @@ import { Translate } from "../components/Translation"
 import { useIntl } from 'react-intl';
 import { useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
+import Link from 'next/link';
 
-type Question = {
-  question: string,
-  answer: string
-}
 
 const FAQ:NextPage = () => {
   const intl = useIntl()
   const [open, setOpen] = useState<number|null>(null)
+  const [openTeacher, setOpenTeacher] = useState<number|null>(null)
 
   const handleClick = (i:number|null) => {
     setOpen(i)
+    setOpenTeacher(null)
+  }
+  const handleClickTeacher = (i:number|null) => {
+    setOpenTeacher(i)
+    setOpen(null)
   }
 
-  const questions:Question[] = [
-    {
-      question: 'QUESTION N° 1',
-      answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni quo enim optio quidem, ex ipsa dolores facilis, dolore, blanditiis iusto esse. Voluptatum aspernatur a dolores deserunt quibusdam. Voluptatum, minus obcaecati. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non vitae sit, itaque iure cupiditate totam quo reiciendis. Ipsa, quos pariatur dolorum sed sit delectus quae saepe, porro aliquid ullam itaqu'
-    },
-    {
-      question: 'QUESTION N° 2',
-      answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni quo enim optio quidem, ex ipsa dolores facilis, dolore, blanditiis iusto esse. Voluptatum aspernatur a dolores deserunt quibusdam. Voluptatum, minus obcaecati. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non vitae sit, itaque iure cupiditate totam quo reiciendis. Ipsa, quos pariatur dolorum sed sit delectus quae saepe, porro aliquid ullam itaqu'
-    },
-    {
-      question: 'QUESTION N° 3',
-      answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni quo enim optio quidem, ex ipsa dolores facilis, dolore, blanditiis iusto esse. Voluptatum aspernatur a dolores deserunt quibusdam. Voluptatum, minus obcaecati. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non vitae sit, itaque iure cupiditate totam quo reiciendis. Ipsa, quos pariatur dolorum sed sit delectus quae saepe, porro aliquid ullam itaqu'
-    },
-    {
-      question: 'QUESTION N° 4',
-      answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni quo enim optio quidem, ex ipsa dolores facilis, dolore, blanditiis iusto esse. Voluptatum aspernatur a dolores deserunt quibusdam. Voluptatum, minus obcaecati. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non vitae sit, itaque iure cupiditate totam quo reiciendis. Ipsa, quos pariatur dolorum sed sit delectus quae saepe, porro aliquid ullam itaqu'
-    },
-    {
-      question: 'QUESTION N° 5',
-      answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni quo enim optio quidem, ex ipsa dolores facilis, dolore, blanditiis iusto esse. Voluptatum aspernatur a dolores deserunt quibusdam. Voluptatum, minus obcaecati. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non vitae sit, itaque iure cupiditate totam quo reiciendis. Ipsa, quos pariatur dolorum sed sit delectus quae saepe, porro aliquid ullam itaqu'
-    },
-    {
-      question: 'QUESTION N° 6',
-      answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni quo enim optio quidem, ex ipsa dolores facilis, dolore, blanditiis iusto esse. Voluptatum aspernatur a dolores deserunt quibusdam. Voluptatum, minus obcaecati. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non vitae sit, itaque iure cupiditate totam quo reiciendis. Ipsa, quos pariatur dolorum sed sit delectus quae saepe, porro aliquid ullam itaqu'
-    },
-    {
-      question: 'QUESTION N° 7',
-      answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni quo enim optio quidem, ex ipsa dolores facilis, dolore, blanditiis iusto esse. Voluptatum aspernatur a dolores deserunt quibusdam. Voluptatum, minus obcaecati. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non vitae sit, itaque iure cupiditate totam quo reiciendis. Ipsa, quos pariatur dolorum sed sit delectus quae saepe, porro aliquid ullam itaqu'
-    },
-    {
-      question: 'QUESTION N° 8',
-      answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni quo enim optio quidem, ex ipsa dolores facilis, dolore, blanditiis iusto esse. Voluptatum aspernatur a dolores deserunt quibusdam. Voluptatum, minus obcaecati. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non vitae sit, itaque iure cupiditate totam quo reiciendis. Ipsa, quos pariatur dolorum sed sit delectus quae saepe, porro aliquid ullam itaqu'
-    },
-    {
-      question: 'QUESTION N° 9',
-      answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni quo enim optio quidem, ex ipsa dolores facilis, dolore, blanditiis iusto esse. Voluptatum aspernatur a dolores deserunt quibusdam. Voluptatum, minus obcaecati. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non vitae sit, itaque iure cupiditate totam quo reiciendis. Ipsa, quos pariatur dolorum sed sit delectus quae saepe, porro aliquid ullam itaqu'
-    },
-    {
-      question: 'QUESTION N° 10',
-      answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni quo enim optio quidem, ex ipsa dolores facilis, dolore, blanditiis iusto esse. Voluptatum aspernatur a dolores deserunt quibusdam. Voluptatum, minus obcaecati. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non vitae sit, itaque iure cupiditate totam quo reiciendis. Ipsa, quos pariatur dolorum sed sit delectus quae saepe, porro aliquid ullam itaqu'
-    },
-  ]
+  const studentQuestions:number[] = [1,2,3,4,5,6,7]
+  const teacherQuestions:number[] = [1,2,3,4,5,6,7,8]
+
   return (
     <>
       <Head>
@@ -74,7 +38,7 @@ const FAQ:NextPage = () => {
           </h1>
         </div>
         <section className="flex flex-col gap-5 my-3">
-          {questions.map((question, i) => (
+          {studentQuestions.map((x, i) => (
             <motion.div 
               key={i} 
               onClick={() => handleClick(i === open ? null : i)}
@@ -83,7 +47,7 @@ const FAQ:NextPage = () => {
               <div className="font-semibold text-lg flex justify-between my-2">
                 <div>
                   <span className={i === open ? 'font-bold text-green-600' : ''}>
-                    {question.question}
+                    {intl.formatMessage({id: `student_question_${i+1}`})}
                   </span>
                 </div>
                 <div>
@@ -92,7 +56,7 @@ const FAQ:NextPage = () => {
                   </span>
                 </div>
               </div>
-              <div className="px-4">
+              <div className="px-4 overflow-clip">
                 <AnimatePresence initial={false}>
                   {
                     i === open && (
@@ -105,15 +69,63 @@ const FAQ:NextPage = () => {
                           open:{opacity: 1, height: "auto"},
                           collapsed:{opacity: 0, height: 0}
                         }}
-                        transition={{duration: 0.5}}>
+                        transition={{duration: 0.2}}>
                           <motion.div
-                            variants={{
-                              collapsed: {scaleY: 0},
-                              open: {scaleY: 1}
-                            }}
                             transition={{duration: 0.7}}>
                               <p>
-                                {question.answer}
+                                {intl.formatMessage({id: `student_answer_${i+1}`},{bold:<b>Profepedia</b>,br:<br/>})}
+                              </p>
+                          </motion.div>
+                      </motion.article>
+                    )
+                  }
+                </AnimatePresence>
+              </div>
+            </motion.div>
+          ))}
+        </section>
+        <div className="text-center my-2">
+          <h1 className="font-semibold text-3xl">
+            <Translate label="faq_title_2"/>
+          </h1>
+        </div>
+        <section className="flex flex-col gap-5 my-3">
+          {teacherQuestions.map((x, i) => (
+            <motion.div 
+              key={i} 
+              onClick={() => handleClickTeacher(i === openTeacher ? null : i)}
+              initial={false}
+              className="px-4 py-6 surface-item shadow-md rounded-lg">
+              <div className="font-semibold text-lg flex justify-between my-2">
+                <div>
+                  <span className={i === openTeacher ? 'font-bold text-green-600' : ''}>
+                    {intl.formatMessage({id: `teacher_question_${i+1}`})}
+                  </span>
+                </div>
+                <div>
+                  <span>
+                    <i className={`bi bi-chevron-${i === openTeacher ? 'up' : 'down'}`}/>
+                  </span>
+                </div>
+              </div>
+              <div className="px-4 overflow-clip">
+                <AnimatePresence initial={false}>
+                  {
+                    i === openTeacher && (
+                      <motion.article
+                        key={`ans_${i}`}
+                        initial="collapsed"
+                        animate="open"
+                        exit="collapsed"
+                        variants={{
+                          open:{opacity: 1, height: "auto"},
+                          collapsed:{opacity: 0, height: 0}
+                        }}
+                        transition={{duration: 0.2}}>
+                          <motion.div
+                            transition={{duration: 0.7}}>
+                              <p>
+                                {intl.formatMessage({id: `teacher_answer_${i+1}`}, {br: <br/>})}
                               </p>
                           </motion.div>
                       </motion.article>
@@ -126,11 +138,11 @@ const FAQ:NextPage = () => {
         </section>
         <div className="text-center my-4">
           <h1 className="font-semibold text-3xl">
-            <Translate label="faq_title"/>
+            <Translate label="faq_title_3"/>
           </h1>
           <div className="my-2 px-2 md:px-0">
             <p className="muted font-medium">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, quidem nemo, exercitationem nihil consequatur, minima dolorem sit esse architecto non nostrum cumque fugit eligendi dolorum impedit doloremque numquam iure voluptas.
+              {intl.formatMessage({id: "faq_paragraph_3"}, {contact: <Link href="/contacto"><a className="text-success hover:text-success">{intl.formatMessage({id: "contact_page"})}</a></Link>})}
             </p>
           </div>
         </div>
